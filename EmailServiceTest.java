d38509b315587c064fbb338807ea8ac8444e6b65
anuragpandeyttn <114070637+anuragpandeyttn@users.noreply.github.com>
2022-10-21 14:30:12 +0530

    Add files via upload

---
+++ EmailServiceTest.java
@@ -0,0 +1,27 @@
+package com.demo.service;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+import com.demo.domain.Order;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EmailServiceTest {
+    
+    @InjectMocks
+    EmailService emailService;
+    
+    @Test
+    public void sendEmailTest() {
+        Order o1 = new Order(5, "biscuits" ,34 );
+        boolean b = emailService.sendEmail(o1,"Anurag Pandey");
+        assertTrue(b);
+    }
+    
+    @Test(expected = RuntimeException.class)
+    public void setcheckmailException() {
+        emailService.sendEmail(new Order());
+    }
+}
